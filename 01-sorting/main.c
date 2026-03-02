0db99ba1fec4d3aa37e4e4b561e3cf94b7f925d0
Jude Alavi <s335041@studenti.polito.it>
2026-03-02 16:12:26 +0100

    Create sorting project structure

---
+++ 01-sorting/main.c
@@ -0,0 +1,10 @@
+// Sorting Project - Politecnico di Torino
+// Original lab exercise - will be refactored and documented
+
+#include <stdio.h>
+
+int main() {
+    return 0;
+}
+
+Create sorting project structure
