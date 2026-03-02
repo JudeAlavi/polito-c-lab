7261fd115098e5a87ce8d3c9dde8b12697a41448
Jude Alavi <s335041@studenti.polito.it>
2026-03-02 16:32:40 +0100

    Add sorting techniques: insertion and bubble sort
    Implemented insertion sort and bubble sort algorithms in C.

---
+++ projects/01-Algorithms-and-Optimization/Sorting-Techniques.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i], j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int data[] = {64, 25, 12, 22, 11};
+    int n = sizeof(data) / sizeof(data[0]);
+    
+    insertionSort(data, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) printf("%d ", data[i]);
+    return 0;
+}
